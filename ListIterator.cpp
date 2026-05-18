484d78cac82824ca914de69348aae7dc61e8aa9a
HunorDomokos <hunihuni250@gmail.com>
2026-05-18 13:13:46 +0300

    added stack for iterating

--- ListIterator.cpp
+++ ListIterator.cpp
@@ -4,26 +4,55 @@
 
 using namespace std;
 
+void ListIterator::push(SortedIndexedList::Node* node) {
+	top = new StackNode(node, top);
+}
+
+SortedIndexedList::Node* ListIterator::pop() {
+	StackNode* tmp = top;
+	SortedIndexedList::Node* data = tmp->data;
+	top = top->next;
+	delete tmp;
+	return data;
+}
+
+void ListIterator::clearStack() {
+	while (top != nullptr) pop();
+}
+
+void ListIterator::pushLeft(SortedIndexedList::Node* node) {
+	while (node != nullptr) {
+		push(node);
+		node = node->left;
+	}
+}
+
 ListIterator::ListIterator(const SortedIndexedList& list) : list(list) {
-	current = 0;
+	top = nullptr;
+	current = nullptr;
+	pushLeft(list.root);
+	if (top != nullptr) current = pop();
 }
 
 void ListIterator::first() {
-	current = 0;
+	clearStack();
+	current = nullptr;
+	pushLeft(list.root);
+	if (top != nullptr) current = pop();
 }
 
 void ListIterator::next() {
 	if (!valid()) throw exception();
-	current++;
+	pushLeft(current->right);
+	if (top != nullptr) current = pop();
+	else current = nullptr;
 }
 
 bool ListIterator::valid() const {
-	return current < list.nr_elems;
+	return current != nullptr;
 }
 
 TComp ListIterator::getCurrent() const {
 	if (!valid()) throw exception();
-	return list.getElement(current);
+	return current->data;
 }
-
-
--- ListIterator.h
+++ ListIterator.h
@@ -3,19 +3,29 @@
 
 
 //DO NOT CHANGE THIS PART
-class ListIterator{
+class ListIterator {
 	friend class SortedIndexedList;
 private:
 	const SortedIndexedList& list;
 	ListIterator(const SortedIndexedList& list);
 
-	int current;
+	struct StackNode {
+		SortedIndexedList::Node* data;
+		StackNode* next;
+		StackNode(SortedIndexedList::Node* d, StackNode* n) : data(d), next(n) {}
+	};
+
+	StackNode* top;
+	SortedIndexedList::Node* current;
+
+	void push(SortedIndexedList::Node* node);
+	SortedIndexedList::Node* pop();
+	void pushLeft(SortedIndexedList::Node* node);
+	void clearStack();
 
 public:
 	void first();
 	void next();
 	bool valid() const;
-    TComp getCurrent() const;
+	TComp getCurrent() const;
 };
-
-
